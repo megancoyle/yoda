2cd0a080cc65f2990ee41f86441a0ffd87da5048
Megan Coyle <meg@mcoyle.com>
2016-08-29 09:57:56 -0400

    Add awkward silence function

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 var translateField  = document.getElementById("translate");
 var translation = document.getElementById("yoda");
 
+var timer = null;
+  // after five seconds, initiate awkward silence
+timer = setTimeout(function(){
+  translation.innerHTML = "...";
+}, 5000);
+
 translateField.onkeypress = function(e){
+
   // when user presses enter
   if (e.keyCode == 13) {
+    // clear timeout
+    if (timer) {
+      clearTimeout(timer);
+      timer = null;
+    }
     var userInput = document.getElementById("translate").value;
     $.ajax({
       url: "https://yoda.p.mashape.com/yoda?sentence=" + userInput,
       success: function(response) {
-        translation.innerHTML = translation.innerHTML + response;
+        translation.innerHTML = response;
+        console.log(timer)
       },
       headers: {
         "X-Mashape-Key": "OGPgEThqxtmshCpTDZyOUBndDjLLp1Lm0qcjsnpxxdQawmnaj7"
